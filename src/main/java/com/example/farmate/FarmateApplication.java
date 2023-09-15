5d95050a6424334f85d9cbe29ceaa3b6a64d8155
jminkkk <minl741@naver.com>
2023-09-15 21:06:19 +0900

    초기 세팅

---
+++ src/main/java/com/example/farmate/FarmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farmate/FarmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
